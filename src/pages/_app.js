c4efd97d339a6134c03f90358e70a496a2062af5
ss-vainik <vainik.kakadiya@sculptsoft.com>
2023-05-03 13:42:12 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+const roboto = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "900",  "700"],
+})
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
